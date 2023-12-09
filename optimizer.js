2cade0ada54fc5a49c31cb9e32749ec2d09bc2c4
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-12-09 08:48:23 +0700

    Available features: auto-generate thumbnail for uploaded videos, processing multiple-size images for future usages.

---
+++ optimizer.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+var processPath = path.resolve(process.cwd(), "optimizer_processes");
+var uploadPath = path.resolve(process.cwd(), "uploads");
+var tinyWidth = 100;
+var smallWidth = 320;
+var mediumWidth = 640;
+
+var optimizer = module.exports = {
+    processes: [],
+    addProcess: function (filename) {
+        return new Promise((resolve, reject) => {
+            filename += ".txt";
+            fs.writeFileSync(path.resolve(processPath, filename), '');
+            optimizer.runProcess(filename)
+            resolve();
+        })
+    },
+    runProcess: function (txtFileName) {
+        if (optimizer.processes.includes(txtFileName) || !fs.existsSync(path.resolve(processPath, txtFileName))) {
+            return;
+        }
+
+        optimizer.processes.push(txtFileName);
+
+        var realFileName = txtFileName.substring(0, txtFileName.lastIndexOf('.'));
+        var buffer = fs.readFileSync(path.resolve(uploadPath, realFileName));
+        var fileExt = realFileName.substring(realFileName.lastIndexOf('.') + 1);
+        var realFileNameWithoutExt = realFileName.substring(0, realFileName.lastIndexOf('.'));
+
+
+        const tinyProcess = new Promise((resolve, reject) => {
+            sharp(buffer)
+                .resize(tinyWidth)
+                .toFile(path.resolve(uploadPath, realFileNameWithoutExt + "_tiny" + "." + fileExt))
+                .then(() => {
+                    resolve();
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+
+        const smallProcess = new Promise((resolve, reject) => {
+            sharp(buffer)
+                .resize(smallWidth)
+                .toFile(path.resolve(uploadPath, realFileNameWithoutExt + "_small" + "." + fileExt))
+                .then(() => {
+                    resolve();
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+
+        const mediumProcess = new Promise((resolve, reject) => {
+            sharp(buffer)
+                .resize(mediumWidth)
+                .toFile(path.resolve(uploadPath, realFileNameWithoutExt + "_medium" + "." + fileExt))
+                .then(() => {
+                    resolve();
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+
+        return Promise.all([tinyProcess, smallProcess, mediumProcess])
+            .then(() => {
+                optimizer.processes.splice(optimizer.processes.indexOf(txtFileName), 1);
+                fs.unlinkSync(path.resolve(processPath, txtFileName));
+            })
+            .catch(err => {
+                console.log(err);
+                optimizer.processes.splice(optimizer.processes.indexOf(txtFileName), 1);
+
+                if (fs.existsSync(path.resolve(processPath, txtFileName))) {
+                    fs.unlinkSync(path.resolve(processPath, txtFileName));
+                }
+            });
+    }
+}
---
+++ routes/index.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const optimizer = require('../optimizer');
+
+const
+  ffmpegPath = require("@ffmpeg-installer/ffmpeg").path,
+  ffprobePath = require("@ffprobe-installer/ffprobe").path,
+  ffmpeg = require("fluent-ffmpeg");
+
+ffmpeg.setFfprobePath(ffprobePath);
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const uploadPath = path.resolve(process.cwd(), "uploads");
+
+const uploader = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + "_" + Math.floor(Math.random() * 100000000000000) + "_" + Math.floor(Math.random() * 100000000000000) + path.extname(file.originalname));
+    }
+  })
+});
+
+router.post('/store', uploader.array("files"), async function (req, res, next) {
+  for (var i = 0; i < req.files.length; i++) {
+    var imageFileName = req.files[i].filename;
+
+    if (/\.(mp4|avi|mov|mkv|wmv)$/i.test(req.files[i].filename)) {
+      imageFileName = req.files[i].filename.substring(0, req.files[i].filename.lastIndexOf('.')) + ".jpg";
+
+      await new Promise((resolve, reject) => {
+        ffmpeg(path.resolve(uploadPath, req.files[i].filename))
+          .thumbnail({
+            // get first frame only
+            timestamps: [0],
+            filename: imageFileName,
+            folder: uploadPath,
+          }).on("end", () => {
+            resolve();
+          })
+      });
+    }
+
+    optimizer.addProcess(imageFileName);
+  }
+
+
+
+  res.json(req.files.map(file => file.filename));
+});
+
+router.get('/:filename', function (req, res, next) {
+  var filename = req.params.filename;
+  var type = req.query.type;
+
+  if (/\.(mp4|avi|mov|mkv|wmv)$/i.test(filename) && type == 'thumbnail') {
+    filename = filename.substring(0, filename.lastIndexOf('.')) + ".jpg";
+  }
+
+  if (/\.(mp4|avi|mov|mkv|wmv)$/i.test(filename)) {
+    res.sendFile(path.resolve(uploadPath, filename));
+  } else {
+    var size = req.query.size;
+
+    if (size == "small" && fs.existsSync(path.resolve(uploadPath, filename.substring(0, filename.lastIndexOf('.')) + "_small" + filename.substring(filename.lastIndexOf('.'))))) {
+      filename = filename.substring(0, filename.lastIndexOf('.')) + "_small" + filename.substring(filename.lastIndexOf('.'));
+    } else if (size == "medium" && fs.existsSync(path.resolve(uploadPath, filename.substring(0, filename.lastIndexOf('.')) + "_medium" + filename.substring(filename.lastIndexOf('.'))))) {
+      filename = filename.substring(0, filename.lastIndexOf('.')) + "_medium" + filename.substring(filename.lastIndexOf('.'));
+    }
+
+    res.sendFile(path.resolve(uploadPath, filename));
+  }
+});
+
+module.exports = router;
